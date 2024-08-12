1f983ee4ead575c9b5e8fc02688e3b731cf6d9d2
lu4ckex <lu4ckex@outlook.com>
2024-08-12 15:42:59 -0300

    Inclusão da descrição do script e adição da versão

--- Calculo do Circulo.py
+++ Calculo do Circulo.py
@@ -1,5 +1,5 @@
 pi = 3.14
 raio = 10
-area = pi * (raio * raio) #Área de um circulo = pi x raio²
+area = pi * (raio * raio) #Área de um elipse = pi x raio²
 
 print ('A área de um circulo com raio de 10 cm é: ', area)
